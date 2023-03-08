59431c25bf03aedc1bf7ee190a32700b047b7743
ali <dlfsh>
2023-03-08 12:34:06 +0330

    Iam Desgin This is Login , Register , Chat

--- ChatMessangerOne/src/main/java/org/example/view/BlockView.java
+++ ChatMessangerOne/src/main/java/org/example/view/BlockView.java
@@ -40,13 +40,14 @@ public class BlockView {
         {
             ChatApplication.setBackground(Color.white);
             ChatApplication.setForeground(Color.white);
-            ChatApplication.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing
-            . border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing .border . TitledBorder
-            . CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dialo\u0067", java .
-            awt . Font. BOLD ,12 ) ,java . awt. Color .red ) ,ChatApplication. getBorder () ) )
-            ; ChatApplication. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
-            ) { if( "borde\u0072" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } )
-            ;
+            ChatApplication.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder (
+            new javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn"
+            , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM
+            , new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 )
+            ,java . awt. Color .red ) ,ChatApplication. getBorder () ) ); ChatApplication. addPropertyChangeListener(
+            new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
+            ) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
+            ;} } );
             ChatApplication.setLayout(null);
 
             //======== panel2 ========
